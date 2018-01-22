ee768215431c9c5c29e6553d6384ab54125bb65d
Maureen Pittman <student@otp588507ots.ucsf.edu>
2018-01-22 15:33:13 -0800

    reverted bad cleanup; added code to count assignments and conditionals

--- test/test_algs.py
+++ test/test_algs.py
@@ -3,57 +3,57 @@ from sort import algs
 
 
 def test_bubblesort():
-    
+
     # 1) Test odd-sized vector + duplicate values
-    assert algs.bubblesort([1,2,4,0,1]) == [0,1,1,2,4]
+    assert algs.bubblesort([1,2,4,0,1])[1] == [0,1,1,2,4]
 
     # 2) Test even+duplicate values
-    assert algs.bubblesort([1,2,4,6,0,1]) == [0,1,1,2,4,6]
+    assert algs.bubblesort([1,2,4,6,0,1])[1] == [0,1,1,2,4,6]
 
     # 3) Test empty vector
-    assert algs.bubblesort([]) == []
+    assert algs.bubblesort([])[1] == []
 
     # 4) Test single-element vectors
-    assert algs.bubblesort([1]) == [1]
+    assert algs.bubblesort([1])[1] == [1]
 
     # 5) Test single-value vectors
-    assert algs.bubblesort([1,1,1,1,1,1,1,1]) == [1,1,1,1,1,1,1,1]
+    assert algs.bubblesort([1,1,1,1,1,1,1,1])[1] == [1,1,1,1,1,1,1,1]
 
     # 6) Test vectors with negative values
-    assert algs.bubblesort([-2,-6,8,9,-4]) == [-6,-4,-2,8,9]
+    assert algs.bubblesort([-2,-6,8,9,-4])[1] == [-6,-4,-2,8,9]
 
     # 7) Test ordered and reverse-order lists of large size
-    assert algs.bubblesort(range(1000)) == range(1000)
-    assert algs.bubblesort(list(reversed(range(1000)))) == list(range(1000))
+    assert algs.bubblesort(range(1000))[1] == range(1000)
+    assert algs.bubblesort(list(reversed(range(1000))))[1] == list(range(1000))
 
     # 8) Test vector of strings
-    assert algs.bubblesort(["will", "this", "work"]) == ["this", "will", "work"]
+    assert algs.bubblesort(["will", "this", "work"])[1] == ["this", "will", "work"]
 
 
 
 def test_quicksort():
 
     # 1) Test odd-sized vector + duplicate values
-    assert algs.quicksort([1,2,4,0,1]) == [0,1,1,2,4]
+    assert algs.quicksort([1,2,4,0,1])[1] == [0,1,1,2,4]
 
     # 2) Test even+duplicate values
-    assert algs.quicksort([1,2,4,6,0,1]) == [0,1,1,2,4,6]
+    assert algs.quicksort([1,2,4,6,0,1])[1] == [0,1,1,2,4,6]
 
     # 3) Test empty vector
-    assert algs.quicksort([]) == []
+    assert algs.quicksort([])[1] == []
 
     # 4) Test single-element vectors
-    assert algs.quicksort([1]) == [1]
+    assert algs.quicksort([1])[1] == [1]
 
     # 5) Test single-value vectors
-    assert algs.quicksort([1,1,1,1,1,1,1,1]) == [1,1,1,1,1,1,1,1]
+    assert algs.quicksort([1,1,1,1,1,1,1,1])[1] == [1,1,1,1,1,1,1,1]
 
     # 6) Test vectors with negative values
-    assert algs.quicksort([-2,-6,8,9,-4]) == [-6,-4,-2,8,9]
+    assert algs.quicksort([-2,-6,8,9,-4])[1] == [-6,-4,-2,8,9]
 
     # 7) Test ordered and reverse-order lists of large size
-    assert algs.quicksort(range(1000)) == list(range(1000))
-    assert algs.quicksort(list(reversed(range(1000)))) == list(range(1000))
+    assert algs.quicksort(range(1000))[1] == list(range(1000))
+    assert algs.quicksort(list(reversed(range(1000))))[1] == list(range(1000))
 
     # 8) Test vector of strings
-    assert algs.quicksort(["will", "this", "work"]) == ["this", "will", "work"]
+    assert algs.quicksort(["will", "this", "work"])[1] == ["this", "will", "work"]
